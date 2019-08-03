14a04a83b9603c54db6a2d41a003dc2ef6751faf
= <=>
2019-08-03 17:31:29 -0300

    [up] conectando com watson

--- src/config/routes.js
+++ src/config/routes.js
@@ -14,7 +14,8 @@ module.exports = function (server) {
 
     // rota de inserção (manualmente construida) de mensagem no banco de dados chatService
     api.post('/chat', (req, res) => { // http://localhost:3003/api/chat (POST)
-
+        console.log('Requisicao');
+        
         req.body.message = req.body.message ? req.body.message : '';
 
         // Chama a função que vai analisar a mensagem do usuário, e responder
@@ -25,7 +26,7 @@ module.exports = function (server) {
             }
         })
             .then((user) => { // Assim que a função finalizar, ela vai retornar o objeto novo de user, e toda a resposta do watson nova.
-
+                
                 // Salva o novo objeto do usuário no banco de dados que contém todas as repostas do watson.
                 user.save((err) => {
                     if (err) {
